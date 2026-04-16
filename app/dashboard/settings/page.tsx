348599fb87f811b2bf387ebf9df4a7533f52ac29
AnilDevchhetri <chhetrivision@gmail.com>
2026-04-16 22:54:53 +0900

    7:20

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 import TeamSection from '@/components/dashboard/settings/teamSection';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Label } from '@/components/ui/label'
+import { Trash2 } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 
 interface OrganizationData {
@@ -79,6 +82,56 @@ const SettingsPage = () => {
                 </CardContent>
             </Card>
             <TeamSection />
+            <Card className='border-red-500/10 bg-red-500/2'>
+                <CardHeader>
+                    <CardTitle className='text-base font-medium text-red-500'>
+                        Danger Zone
+                    </CardTitle>
+                    <CardDescription className='text-red-500/60'>
+                        Irreversible actions for this workspace
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className='flex items-center justify-between'>
+                        <div className='space-y-0.5'>
+                            <p className='text-sm font-medium text-zinc-300'>
+                                Delete WorkSpace
+                            </p>
+                            <p className='text-xs text-zinc-500'>
+                                Permanently Delete all knowlege, converstion, and settings.
+                            </p>
+                        </div>
+                        <AlertDialog>
+                            <AlertDialogTrigger asChild>
+                                <Button variant={"destructive"}
+                                    className='bg-red-500/20 text-red-500 hover:bg-red-500/10 border border-red-500/20 shadow-none'
+                                >
+                                    <Trash2 className='w-4 h-4 mr-2' />
+                                    Delete
+                                </Button>
+                            </AlertDialogTrigger>
+                            <AlertDialogContent className='bg-[#0E0E12] border-white/10'>
+                                <AlertDialogHeader>
+                                    <AlertDialogTitle className='text-white'>
+                                        Are You Sure?
+                                    </AlertDialogTitle>
+                                    <AlertDialogDescription className='text-zinc-400'>
+                                        This action cannot be undone. This will permanently delte your workspace and remove all assoicated data from our servers.
+                                    </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter className='bg-transparent border-0'>
+                                    <AlertDialogCancel className='bg-transparent border-white/10 text-zinc-300 hover:bg-white/5 hover:text-white '>
+                                        Cancel
+                                    </AlertDialogCancel>
+                                    <AlertDialogAction className='bg-red-500 text-white hover:bg-red-600 border-none'>
+                                        Delete Workspace
+                                    </AlertDialogAction>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+                    </div>
+                </CardContent>
+            </Card>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -26,4 +26,4 @@ const Page = () => {
 
 export default Page
 
-//7:17
+//7:20
