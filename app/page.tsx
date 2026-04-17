8f7f8b2479a5632640230eb0d27bd10d65bff3ce
AnilDevchhetri <chhetrivision@gmail.com>
2026-04-18 00:23:21 +0900

    7:31

--- app/page.tsx
+++ app/page.tsx
@@ -26,4 +26,4 @@ const Page = () => {
 
 export default Page
 
-//7:20
+//7:31
---
+++ public/widget.js
@@ -0,0 +1,61 @@
+(function () {
+    try {
+        var script = document.currentScript;
+        if (!script) return;
+        var widgetId = script.getAttribut("data-id");
+        if (!widgetId) {
+            console.error("[TasukeAi] Missing data-id");
+            return;
+        }
+
+        fetch("http://localhost:3000/api/widget/session", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "omit",
+            body: JSON.stringify({
+                widget_it: widgetId,
+            }),
+        }).then(function (res) {
+            if (!res.ok) throw new Error("Session request failded");
+        }).then(function (data) {
+            if (!data || !data.token) {
+                throw new Error("Invalid Session response.")
+            }
+
+            var iframe = document.createElement("iframe");
+            iframe.src = "http://localhost:3000/embed?token" + encodeURIComponent(data.token)
+            iframe.setAttribute("title", "Support Chat");
+            iframe.style.position = "fixed";
+            iframe.style.bottom = "20px";
+            iframe.style.right = "20px";
+            iframe.style.width = "60px";
+            iframe.style.height = "none";
+            iframe.style.zIndex = "999999";
+            iframe.style.borderRadius = "30px";
+            iframe.style.background = "transparent";
+            iframe.style.transition = "all 0.3s ease";
+
+            document.body.appendChild(iframe);
+
+            window.addEventListener("message", function (event) {
+                if (event.data && event.data.type === "resize") {
+                    iframe.style.width = event.data.width;
+                    iframe.style.height = event.data.height;
+                    iframe.style.borderRadius = event.data.borderRadius || "12px";
+
+                    if (event.data.boxShadow) {
+                        iframe.style.boxShadow = event.data.boxShadow;
+                    }
+                }
+            })
+
+        }).catch(function (err) {
+            console.error("[TasukeAi] failed to load widget: ", err)
+        })
+
+    } catch (error) {
+        console.error("[TasukeAi] failed to load widget: ", error)
+    }
+})();
